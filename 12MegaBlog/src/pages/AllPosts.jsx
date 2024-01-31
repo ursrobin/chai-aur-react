970c93c0d488fc1e0490918cfdca1449e3125d09
Robin <67267098+ursrobin@users.noreply.github.com>
2024-01-31 19:15:45 +0530

    Fixed post fetching
    Fixed post fetching in the component by moving the post retrieval function(getposts) into the useEffect hook, resolving performance issues caused by unnecessary reloads due to React behavior

--- 12MegaBlog/src/pages/AllPosts.jsx
+++ 12MegaBlog/src/pages/AllPosts.jsx
@@ -3,13 +3,14 @@ import { Container, PostCard } from '../components'
 import appwriteService from "../appwrite/config";
 
 function AllPosts() {
-    const [posts, setPosts] = useState([])
-    useEffect(() => {}, [])
-    appwriteService.getPosts([]).then((posts) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+      appwriteService.getPosts([]).then((posts) => {
         if (posts) {
-            setPosts(posts.documents)
+          setPosts(posts.documents);
         }
-    })
+      });
+    }, []);
   return (
     <div className='w-full py-8'>
         <Container>
